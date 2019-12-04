4135c0120b07c4e4cd283f9f754ec290fd9ee965
Kevin Childs <kevinicus37@gmail.com>
2019-12-04 09:19:13 -0600

    Completed form verification working.

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function(){
+   const pilot = document.getElementById("pilotName");
+   const copilot = document.getElementById("copilotName");
+   const fuelLevel = document.getElementById("fuelLevel");
+   const cargoMass = document.getElementById("cargoMass");
+   const form = document.querySelector("form");
+
+   form.addEventListener("submit", function(event){
+      if (pilot.value === "" || copilot.value === "" || fuelLevel.value === "" || cargoMass.value === ""){
+         alert("All fields are required.");
+         event.preventDefault();
+      }
+   });
+})
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
